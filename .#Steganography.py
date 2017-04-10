7fa56ebeac74101cca4e4967175498b30405bf83
Kent Gauen <gauenk@purdue.edu>
2017-04-10 00:48:16 -0400

    checking

---
+++ .#Steganography.py
@@ -0,0 +1,1 @@
+gauenk@kent-laptop.13114:1491773591
--- Steganography.py
+++ Steganography.py
@@ -5,17 +5,6 @@ from PIL import Image
 import numpy as np
 from timer import Timer ## TODO REMOVE THIS
 
-repl_dict = {77:12,72:7,114:43,109:38,118:47,66:1,103:32,90:25,65:0,85:20,101:30,121:50,112:41,104:33,122:51,120:49,107:36,51:55,83:18,55:59,100:29,86:21,70:5,87:22,88:23,98:27,50:54,99:28,116:45,106:35,71:6,119:48,79:14,74:9,48:52,97:26,54:58,43:62,113:42,76:11,102:31,108:37,105:34,82:17,78:13,49:53,52:56,117:46,80:15,53:57,111:40,67:2,47:63,75:10,110:39,56:60,81:16,57:61,68:3,73:8,69:4,84:19,115:44,89:24}
-#repl_dict = {'A':0,'Q':16,'g':32,'w':48,'B':1,'R':17,'h':33,'x':49,'C':2,'S':18,'i':34,'y':50,'D':3,'T':19,'j':35,'z':51,'E':4,'U':20,'k':36,'0':52,'F':5,'V':21,'l':37,'1':53,'G':6,'W':22,'m':38,'2':54,'H':7,'X':23,'n':39,'3':55,'I':8,'Y':24,'o':40,'4':56,'J':9,'Z':25,'p':41,'5':57,'K':10,'a':26,'q':42,'6':58,'L':11,'b':27,'r':43,'7':59,'M':12,'c':28,'s':44,'8':60,'N':13,'d':29,'t':45,'9':61,'O':14,'e':30,'u':46,'+':62,'P':15,'f':31,'v':47,'/':63}
-
-
-base64index="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/"
-base64chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
-
-
-['A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z', '0', '1', '2', '3','4', '5', '6', '7', '8', '9', '+', '/']
-
 class Payload():
 
 
@@ -51,8 +40,6 @@ class Payload():
     def generate_content(self):
         raw_data = self.unroll()
         #.rfind()
-        _t = Timer()
-        _t.tic()
         if self.compressionLevel >= 0:
             txt_data = np.frombuffer(zlib.compress(raw_data,self.compressionLevel),dtype=np.uint8)
             compressed_str = [ 84, 114, 117, 101] # True in numbers
@@ -60,13 +47,8 @@ class Payload():
             txt_data = raw_data
             compressed_str = [ 70,  97, 108, 115, 101] # False in numbers
 
-        print(np.size(self.img),len(raw_data),len(txt_data))
-        _t.toc()
-        print(_t.average_time,"compression time")
-        #xml_array = self.xml_header + 
+        #print(np.size(self.img),len(raw_data),len(txt_data))
         xml_str = self.make_xml(txt_data,compressed_str)
-        _t = Timer()
-        _t.tic()
         xml_up = np.unpackbits(xml_str)
         padding = np.zeros(6-len(xml_up)%6,dtype=np.uint8)
         if len(padding) == 6:
@@ -74,12 +56,10 @@ class Payload():
         this = np.right_shift(np.packbits(\
                  np.append(xml_up,padding).reshape((len(xml_up)+len(padding))//6,6)\
                  ,axis=1),2).ravel()
-        _t.toc()
-        print(_t.average_time,"string -> b64string")
-        print(this[0:10],"THIS")
-        self.content = np.array(list(this),dtype=np.dtype("uint8"))
+        self.content = this
 
     def reconstruct_img(self):
+
         xml_str = base64.b64decode(self.content).decode("utf-8")
         xml_reg = r"<\?xml version=\"1.0\" encoding=\"UTF-8\"\?><payload type=\"(?P<type>(Color|Gray))\" size=\"(?P<len>[0-9]+),(?P<width>[0-9]+)\" compressed=\"(?P<cp>(True|False))\"\>(?P<img>[0-9, ]+)</payload>"
         match = re.match(xml_reg,xml_str)
@@ -159,10 +139,17 @@ class Carrier():
        1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0,
        0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0,
        1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1]
+        #self.xml_tail = np.array([0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1,
+        # 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1,
+        # 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0,
+        # 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0],dtype=np.uint8)
+        self.color=True
+        if len(np.squeeze(self.img.shape)) < 3:
+            self.color=False
         self.img_shape = self.img.shape
         if len(self.img_shape) < 3:
             self.img_shape.append(1)
-        print(self.img_shape,"carrier image shape.")
+        #print(self.img_shape,"carrier image shape.")
         if type(self.img) is not np.ndarray or np.issubdtype(self.img.dtype,np.uint8) is False:
             raise TypeError("Incorrect parameter type.")
 
@@ -185,17 +172,20 @@ class Carrier():
         # return np.all(test_vect)
     
     def embedPayload(self, payload, override=False):
+        content_len = len(payload.content)
+        img_size = np.size(self.img)
+        if self.color is True:
+            img_size = img_size // 3
         if isinstance(payload,Payload) is False or type(payload.content) is not np.ndarray or type(payload.content.dtype) is False:
             raise TypeError("parameter passed contains an incorrect type.")
-        if len(payload.content) > np.size(self.img):
+        if content_len > img_size:
             # print(len(payload.content))
             # print(np.size(self.img))
             raise ValueError("Payload size is larger than what the carrier can hold.")
         if self.payloadExists() is True and override is False:
             raise Exception("Payload exists and override is False.")
-        _t = Timer()
-        _t.tic()
-        content_len = len(payload.content)
+        #print(content_len)
+        #print(np.size(self.img))
         this = self.img.reshape((self.img_shape[0]*self.img_shape[1],self.img_shape[2]))[:content_len]&252|(np.tile(payload.content[:,np.newaxis],3)&[48,12,3])//[16,4,1]
         # up_img = np.unpackbits(np.copy(self.img))
         # up_cont = np.unpackbits(payload.content)
@@ -204,17 +194,11 @@ class Carrier():
         # up_img[7:len(up_cont)//2*8:8] = up_cont[1::2]
         # up_img = np.packbits(up_img).reshape(self.img_shape[0],self.img_shape[1],self.img_shape[2])
         # up_img = np.squeeze(up_img)
-        _t.toc()
-        print(_t.average_time,"this")
-        _t = Timer()
-        _t.tic()
         that = np.concatenate((this,\
             self.img.reshape(\
             (self.img_shape[0]*self.img_shape[1],self.img_shape[2]))[content_len:]))\
             .reshape(self.img_shape[0],self.img_shape[1],self.img_shape[2])\
             .astype(np.uint8)
-        _t.toc()
-        print(_t.average_time,"that")
         #that = up_img
         # print((np.tile(payload.content[:,np.newaxis],3)&[48,12,3])//[16,4,1],"content")
         # count = 0
@@ -223,9 +207,9 @@ class Carrier():
         #     if count < 5:
         #         count +=1 
         #         print(bin(i[0]),bin(i[1]),bin(i[2]))
-        print(np.unpackbits(self.img[0][0]).reshape(3,8)[:,6:])
-        print(np.unpackbits(that[0][0]).reshape(3,8)[:,6:])
-        print(np.unpackbits(payload.content[0:2]).reshape(2,8)[:,2:])
+        # print(np.unpackbits(self.img[0][0]).reshape(3,8)[:,6:])
+        # print(np.unpackbits(that[0][0]).reshape(3,8)[:,6:])
+        # print(np.unpackbits(payload.content[0:2]).reshape(2,8)[:,2:])
         return that
         
         # list(map(lambda x,y: \
@@ -250,18 +234,28 @@ class Carrier():
         # return this
 
     def extractPayload(self):
-        _t = Timer()
-        _t.tic()
         if self.payloadExists() is False:
             raise Exception("No payload present.")
-        end_elems = ''.join(list(map(lambda x: '{0:08b}'.format(x)[6:8],self.img.ravel())))
-        conv_list = list(map(lambda x: int(x,2),re.findall('....',end_elems)))
-        #end_elems = bytearray(end_elems,"utf-8")
-        _t.toc()
-        #print(end_elems)
-        print(_t.average_time,"to bin")
+        
+        up_img = np.unpackbits(self.img)
+        _mid = np.ravel(np.column_stack((up_img[6::8],up_img[7::8])).astype(np.uint8))
+        if len(_mid) % 6 != 0:
+            print("PROBLEM",len(_mid) % 6)
+        #print(np.packbits(_mid.reshape((len(_mid)//6,6)),axis=1))
+        #print(_mid,"mid")
+        _out = np.right_shift(np.packbits(_mid.reshape((len(_mid)//6,6)),axis=1),2).ravel()
+        #print(_out)
+        #_out = np.packbits(_mid,axis=0)
+        return _out
+        # up_cont = up_cont.reshape(len(up_cont)//8,8)[:,2:].ravel()
 
-        return end_elems
+        # up_img[7:len(up_cont)//2*8:8] = up_cont[1::2]
+
+        #end_elems = ''.join(list(map(lambda x: '{0:08b}'.format(x)[6:8],self.img.ravel())))
+        #conv_list = list(map(lambda x: int(x,2),re.findall('....',end_elems)))
+        #end_elems = bytearray(end_elems,"utf-8")
+
+        #return end_elems
 
     def clean(self):
         rand_vals = np.random.randint(0,255,np.size(self.img),dtype=np.uint8)
@@ -305,24 +299,22 @@ def test_carrier_clean():
 def embed_payload():
     #filename = "./nature.jpg"
 
-    #payload_filename = "./nature.jpg"
-    payload_filename = "./color.png"
-    carrier_filename = "./nature.jpg"
+    payload_filename = "./nature.jpg"
+    #payload_filename = "./color.png"
+    carrier_filename = "./larger_test.jpg"
 
     payload_img = imread(payload_filename)
     carrier_img = imread(carrier_filename)
 
-    _t = {'payload': Timer(),'carrier_i': Timer(),'embed': Timer(),\
-          'carrier_e':Timer(),'payload_exists_i':Timer(),'payload_exists_e':Timer(),\
+    _t = {'payload': Timer(),'embed': Timer(),\
+          'payload_exists_i':Timer(),'payload_exists_e':Timer(),\
           'extract_payload':Timer()}
 
     _t['payload'].tic()
     a = Payload(payload_img,9) ## create content to embed
     _t['payload'].toc()
 
-    _t['carrier_i'].tic()
     b = Carrier(carrier_img)    ## create Carrier instance
-    _t['carrier_i'].toc()
 
     _t['payload_exists_i'].tic()
     print(b.payloadExists() == False)
@@ -336,9 +328,7 @@ def embed_payload():
     #imshow(carrier_img) ## show both images to see the difference
     #imshow(eimg) ## show to see the difference
 
-    _t['carrier_e'].tic()
     b_e = Carrier(eimg) # create carrier WITH embedded image
-    _t['carrier_e'].toc()
 
     _t['payload_exists_e'].tic()
     print(b_e.payloadExists() == True)
@@ -349,7 +339,8 @@ def embed_payload():
     ex_img = b_e.extractPayload()
     _t['extract_payload'].toc()
 
-    print(np.array_equal(ex_img,a.content),"extract") ## extraction should be equal to embedded content
+    idx = range(1790,1804)
+    print(np.array_equal(ex_img[:len(a.content)],a.content),"extract") ## extraction should be equal to embedded content
     
     print_time_dict(_t)
 
